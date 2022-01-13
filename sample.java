2e74cb33f9ab0360c1fc2c89f1ce9246f32d29bc
NIthiwedhniR <68688823+NIthiwedhniR@users.noreply.github.com>
2022-01-13 16:54:32 +0530

    Update sample.java
    date update

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 public class sample {
  public static void main(String args[])	
  {
-	 System.out.println("Excecuting from jenkins ..........");
+	 System.out.println("Excecuting from jenkins on  13/07/2022..........");
  }
 }
